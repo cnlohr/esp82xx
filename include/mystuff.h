79dc1271e93daf3eaa2278a652c874499d2da7fb
Charles Lohr <charles@cnlohr.com>
2016-08-18 23:26:08 -0400

    Update the reflash code to support USB.
    Include fast GPIO code in mystuff.h

--- include/mystuff.h
+++ include/mystuff.h
@@ -69,4 +69,13 @@ char * ICACHE_FLASH_ATTR strdupcaselower( const char * src );
 
 uint32_t ICACHE_FLASH_ATTR GetCurrentIP( );
 
+#define PIN_OUT       ( *((uint32_t*)0x60000300) )
+#define PIN_OUT_SET   ( *((uint32_t*)0x60000304) )
+#define PIN_OUT_CLEAR ( *((uint32_t*)0x60000308) )
+#define PIN_DIR       ( *((uint32_t*)0x6000030C) )
+#define PIN_DIR_OUTPUT ( *((uint32_t*)0x60000310) )
+#define PIN_DIR_INPUT ( *((uint32_t*)0x60000314) )
+#define PIN_IN        ( *((volatile uint32_t*)0x60000318) )
+#define _BV(x) ((1)<<(x))
+
 #endif
