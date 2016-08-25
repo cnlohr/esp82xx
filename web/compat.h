1411afe6141050d73cf35904ef2ac1178ad2229f
CNLohr <charles@cnlohr.com>
2016-08-25 02:53:24 -0400

    Revert "Merge back into dev, where the commit should have been made."

--- web/compat.h
+++
@@ -1,4 +0,0 @@
-// This definition does not exist in OSX, it's a Linuxism
-#ifndef MSG_NOSIGNAL
-  #define MSG_NOSIGNAL 0
-#endif
--- web/pushtodev.c
+++ web/pushtodev.c
@@ -11,7 +11,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <libusb-1.0/libusb.h>
-#include "compat.h"
 
 struct libusb_device_handle *devh = NULL;
 
