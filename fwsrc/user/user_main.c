372ef870648e359913e15878648f2bd7599b85b7
CNLohr <charles@cnlohr.com>
2017-02-18 18:45:03 -0500

    Update user_main.c
    Use recommended setup for cal flash map.

--- fwsrc/user/user_main.c
+++ fwsrc/user/user_main.c
@@ -23,7 +23,7 @@ usr_conf_t * UsrCfg = (usr_conf_t*)(SETTINGS.UserData);
 
 
 void user_rf_pre_init(void) { /*nothing*/ }
-void user_rf_cal_sector_set() { /*nothing*/ }
+
 
 
 char * strcat( char * dest, char * src )
@@ -129,3 +129,37 @@ void user_init(void)
 void EnterCritical() { }
 
 void ExitCritical() { }
+
+
+//For SDK 2.0.0 only.
+uint32 ICACHE_FLASH_ATTR user_rf_cal_sector_set(void)
+{
+    enum flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 8;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
