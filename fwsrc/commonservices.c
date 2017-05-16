860a50bad05be3e43d63f9b37d90afdc8c4b45a7
con-f-use <con-f-use@gmx.net>
2017-05-16 13:07:35 +0200

    Add features to allow for full flash erase and provide init_data for both regular ADC and ADC33. Also, cleanup location of some functions in main.

--- fwsrc/commonservices.c
+++ fwsrc/commonservices.c
@@ -1020,4 +1020,47 @@ void ICACHE_FLASH_ATTR CSSettingsSave()
 }
 
 
+//For SDK 2.0.0 only.
+uint32 ICACHE_FLASH_ATTR user_rf_cal_sector_set(void)
+{
+    enum flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 8;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
+
+
+//For some reason doesn't exist under modern GCC.
+char * strcat( char * dest, char * src )
+{
+    char *rdest = dest;
+    while (*dest) dest++;
+    while (*dest++ = *src++);
+    return rdest;
+}
+
 struct CommonSettings SETTINGS __attribute__ ((aligned (16)));
